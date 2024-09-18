5806f9f82c69fe04377a0306172377c2c93d0f5c
christopher-lee <clee651@terpmail.umd.edu>
2024-09-18 13:00:13 -0400

    Newly added deck now immediately avaiable in deck select, with much less fuss than main branch.

--- src/components/AddEditPage.js
+++ src/components/AddEditPage.js
@@ -1,45 +1,28 @@
 import { useState, useEffect } from 'react';
 
-function getDeckOptions(db) {
-    const deck_options = db.getDeckNames().map((deck_name, index) => {
-        return (
-            <option key={index} value={deck_name}>{deck_name}</option>
-        );
-    });
-
-    return deck_options;
-}
-
-function getChapterOptions(db, deckSelection) {
-    const chapter_options = db.getChapterNames(deckSelection).map((chapter_name, index) => {
-        return (
-            <option key={index} value={chapter_name}>{chapter_name}</option>
-        );
-    });
-
-    return chapter_options;
-}
-
 function PageTitle() {
     return (
         <h2>Add/Edit</h2>
     );
 }
 
-function NewDeckArea({ onAddDeck }) {
+function NewDeckArea({ onDeckAdd, addDeck }) {
+    function handleDeckAdd() {
+        // Get the new deck name
+        const deckName = document.getElementById('new-deck-name');
+        // Add the new deck to the db
+        addDeck(deckName.value);
+        // Reset text input
+        deckName.value = "";
+        // Update deck names
+        onDeckAdd();
+    }
     return (
         <>
             <div className='title-button-layout'>
                 <h3>Deck</h3>
                 <input 
-                    onClick={() => {
-                        // Get the new deck name from input text and add to DB
-                        const id = document.getElementById('new-deck-name').value;
-                        onAddDeck(id);
-                        // Reset text input
-                        const inputText = document.getElementById('new-deck-name');
-                        inputText.value = "";
-                    }}
+                    onClick={handleDeckAdd}
                     type='button'
                     className='button-style-2'
                     value={"Add Deck"}></input>
@@ -50,29 +33,19 @@ function NewDeckArea({ onAddDeck }) {
     );
 }
 
-function NewChapterArea({ db, onAddChapter }) {
+function NewChapterArea({ }) {
     return (
         <div id='new-area'>
             <div className='title-button-layout more-margin'>
                 <h3>Chapter</h3>
                 <input
-                    onClick={() => {
-                        // Get the new chapter name and selected deck
-                        const id = document.getElementById("new-chapter-name");
-                        const target = document.getElementById("deck-select-new-chapt");
-                        const deck_name = target.options[target.selectedIndex].text;
-                        // Add new chapter to DB
-                        onAddChapter(deck_name, id.value);
-                        // Reset text input
-                        id.value = "";
-                    }}
                     type='button'
                     className='button-style-2'
                     value={"Add Chapter"}></input>
             </div>
             <label htmlFor='deck-select'>Select Deck</label>
             <select id='deck-select-new-chapt' name='deck-select'>
-                {getDeckOptions(db)}
+                {}
             </select><br />
             <label htmlFor='new-chapter-name' id='new-chapt-name'>New Chapter Name:</label><br />
             <input autoComplete='off' id='new-chapter-name' type='text'></input><br />
@@ -81,138 +54,49 @@ function NewChapterArea({ db, onAddChapter }) {
 }
 
 function Content({ db }) {
-    const [deckSelection, setDeckSelection] = useState("Deck 0");
-    const [deckOptions, setDeckOptions] = useState(db.getDeckNames());
-    const [chapterOptions, setChapterOptions] = useState(db.getChapterNames(deckSelection));
+    const [deckNames, setDeckNames] = useState([]);
 
-    function handleDeckSelectEditAdd(selected_deck) {
-        setDeckSelection(selected_deck);
+    function addDeck(deckName) {
+        db.addDeck(deckName);
     }
 
-    function handleNewDeck(new_deck_name) {
-        // Add the new deck
-        db.addDeck(new_deck_name);
-        // Updating the state will allow select lists to see the update without page refresh
-        setDeckOptions(db.getDeckNames());
+    function updateDeckNames() {
+        // pull from database to update deckNames state
+        setDeckNames(db.getDeckNames());
     }
 
-    function handleNewChapter(deck_name, new_chapter_name) {
-        // Add the new chapter
-        db.addChapter(deck_name, new_chapter_name);
-        // Updating the state will allow select lists to see the update without page refresh
-        setChapterOptions(db.getChapterNames());
-    }
-
-    function handleNewCard(deck_name, chapter_name, new_card) {
-        // new card must be an object with question, answer properties
-        // db.addCard(deck_name, chapter_name, new_card);
-        console.log(new_card);
-    }
-
-    function handleEditCard(deck_name, chapter_name, id, edit_card) {
-        // db.editCard(deck_name, chapter_name, id, edit_card["question"], edit_card["answer"]);
-        console.log(id, edit_card);
-    }
+    // Load deck names on initial load
+    useEffect(() => {
+        updateDeckNames();
+    }, []);
 
     return (
       <div id='container'>
         <div id='new-chapt-deck'>
-            <NewDeckArea onAddDeck={handleNewDeck}/>
-            <NewChapterArea db={db} onAddChapter={handleNewChapter}/>
+            <NewDeckArea onDeckAdd={updateDeckNames} addDeck={addDeck} />
+            <NewChapterArea />
         </div>
-        <AddEditCardArea db={db} deckSelection={deckSelection} handleDeckSelect={handleDeckSelectEditAdd}/>
+        <AddEditCardArea deckNames={deckNames} />
       </div>  
     );
 }
 
-function AddEditCardArea({ db, deckSelection, handleDeckSelect }) {
-    const [chapterSelection, setChapterSelection] = useState("Chapter 1");
-    const [cardOptions, setCardOptions] = useState([]);
-    const [cards, setCards] = useState([]);
-    const [cardQuestion, setCardQuestion] = useState("");
-    const [cardAnswer, setCardAnswer] = useState("");
-    const [cardSelect, setCardSelect] = useState("0");
+function AddEditCardArea({ deckNames }) {
+    const [deckOptions, setDeckOptions] = useState([]);
 
-    let thisOne = "";
+    function updateDeckOptions() {
+        // Create a new set of deck options
+        const deckOpts = deckNames.map((name, index) => (
+            <option key={index}>{name}</option>
+        ));
 
-    useEffect(() => {
-        const deck_select = document.getElementById('deck-select-edit-add');
-        const deck_selected = deck_select.options[deck_select.selectedIndex];
-        thisOne = deck_selected.value;
-    }, [deckSelection]);
-
-    async function getData() {
-        // Set chapter state based off input element value
-        const chpt_select = document.getElementById('chapter-select');
-        const selected = chpt_select.options[chpt_select.selectedIndex];
-        console.log("this is the selected", selected);
-        setChapterSelection(selected.value);
-
-        // Get the deck from input element value
-        const deck_select = document.getElementById('deck-select-edit-add');
-        const deck_selected = deck_select.options[deck_select.selectedIndex];
-        handleDeckSelect(deck_selected.value);
-        console.log("Deck selection", deck_selected.value, deckSelection);
-
-        // Set card data, if it exists
-        const data = db.getCards(thisOne, selected.value);
-        setCards(data);
-        const results = [];
-
-        if (data != []) {
-            // Store results in the results array
-            console.log("did not enter the no card clause");
-            data.forEach(element => {
-                results.push(
-                    <option key={"Card " + element["id"]} value={element["id"]}>Card {element["id"] + 1}</option>
-                    // <option key={"Card " + element["id"]} value={element}>Card {element["question"]}</option>
-                );
-            });
-        } else {
-            console.log("We entered the no cards clause");
-            results.push(
-                <option key={"No Cards"}>Empty</option>
-            );
-        }
-
-        
-        setCardOptions(results);
-        changeCard();
+        // Update deckOptions state
+        setDeckOptions(deckOpts);
     }
 
-    async function changeCard() {
-        // Get the chapter from input element value
-        const chpt_select = document.getElementById('chapter-select');
-        const selected = chpt_select.options[chpt_select.selectedIndex];
-        
-        // Get the deck from input element value
-        const deck_select = document.getElementById('deck-select-edit-add');
-        const deck_selected = deck_select.options[deck_select.selectedIndex];
-        console.log(deckSelection);
-
-        // flag for new deck
-        let new_deck_flag = db.getChapterNames(deck_selected.value).length == 0;
-
-        // Set the card displays, if cards exist
-        if (db.getChapter(deckSelection, selected.value).size != 0 && !new_deck_flag) {
-            setCardQuestion(db.getCard(deckSelection, selected.value, cardSelect)["question"]);
-            setCardAnswer(db.getCard(deckSelection, selected.value, cardSelect)["answer"]);
-        } else {
-            // if empty chapter or deck, empty card text, select card text, and select chapter text
-            setCardQuestion("");
-            setCardAnswer("");
-        }
-        
-    }
-
-    // Load card data on initial load
     useEffect(() => {
-        getData();
-    }, []);
-    // Update card text on cardSelect state change
-    useEffect(() => {
-        changeCard();
-    }, [cardSelect]);
+        updateDeckOptions();
+    }, [deckNames])
 
     return (
         <div id='edit-add'>
@@ -223,50 +107,33 @@ function AddEditCardArea({ db, deckSelection, handleDeckSelect }) {
             <div className='select-layout'>
                 <label htmlFor='deck-select'>Select Deck</label>
                 <select
-                    onChange={() => {
-                        // On deck select change, update the deckSelection state
-                        const target = document.getElementById("deck-select-edit-add");
-                        handleDeckSelect(target.options[target.selectedIndex].value);
-                        getData();
-                    }}
                     id='deck-select-edit-add'
                     name='deck-select'
-                    className='select-style'>
-                    {getDeckOptions(db)}
+                    className='select-style'
+                >
+                    {deckOptions}
                 </select><br />
             </div>
             <div className='select-layout'>
                 <label htmlFor='chapter-select'>Select Chapter</label>
                 <select 
-                    onChange={() => {
-                        getChapterOptions(db, deckSelection);
-                        getData();
-                    }}
                     id='chapter-select'
                     name='chapter-select' 
                     className='select-style'>
-                    {getChapterOptions(db, deckSelection)}
+                    {}
                 </select><br />
             </div>
             <div className='select-layout'>
                 <label htmlFor='card-select'>Select Card</label>
                 <select 
-                    onChange={(e) => {
-                        setCardSelect(e.target.value);
-                        changeCard();    
-                    }}
                     id='card-select' name='card-select' className='select-style'>
-                    {cardOptions}
+                    {}
                 </select><br />
             </div>
             <label>Question:</label><br />
-            <input autoComplete='off' value={cardQuestion} onChange={(e) => {
-                setCardQuestion(e.target.value);
-            }} type='text' id='card-question-text'></input><br />
+            <input autoComplete='off' type='text' id='card-question-text'></input><br />
             <label>Answer:</label><br />
-            <textarea value={cardAnswer} onChange={(e) => {
-                setCardAnswer(e.target.value);
-            }} id='card-answer-text' name='answer' rows='5' cols='40'></textarea>
+            <textarea id='card-answer-text' name='answer' rows='5' cols='40'></textarea>
         </div>
     );
 }
