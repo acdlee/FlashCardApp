f17906f60341141f5e0f5d2c1e2e6185a818098c
christopher-lee <clee651@terpmail.umd.edu>
2024-09-18 14:17:25 -0400

    A new Deck or Chapter is mirrored in select lists immediately. Check the research.txt file in this commit for a description of how I achieved this for chapter options.

--- src/components/AddEditPage.js
+++ src/components/AddEditPage.js
@@ -33,19 +33,55 @@ function NewDeckArea({ onDeckAdd, addDeck }) {
     );
 }
 
-function NewChapterArea({ }) {
+function NewChapterArea({ db, deckNames, updateChptState }) {
+    const [currentDeck, setCurrentDeck] = useState("Deck 0");
+    const [deckOptions, setDeckOptions] = useState([]);
+
+    function updateDeckOptions() {
+        // Create a new set of deck options
+        const deckOpts = deckNames.map((name, index) => (
+            <option key={index}>{name}</option>
+        ));
+
+        // Update deckOptions state
+        setDeckOptions(deckOpts);
+    }
+
+    function onDeckSelect(e) {
+        // Update state based off select list
+        const selectedDeck = e.target.options[e.target.selectedIndex].value;
+        setCurrentDeck(selectedDeck);
+    }
+
+    function addNewChapter() {
+        // Add the new chapter from the input element to the database
+        const newChapterName = document.getElementById('new-chapter-name').value;
+        db.addChapter(currentDeck, newChapterName);
+        updateChptState(db.getChapterNames(currentDeck));
+    }
+
+    // When deckNames changes, update the deck select options
+    useEffect(() => {
+        updateDeckOptions();
+    }, [deckNames]);
+    
     return (
         <div id='new-area'>
             <div className='title-button-layout more-margin'>
                 <h3>Chapter</h3>
                 <input
+                    onClick={addNewChapter}
                     type='button'
                     className='button-style-2'
                     value={"Add Chapter"}></input>
             </div>
             <label htmlFor='deck-select'>Select Deck</label>
-            <select id='deck-select-new-chapt' name='deck-select'>
-                {}
+            <select
+                onChange={(e) => onDeckSelect(e)}
+                id='deck-select-new-chapt' 
+                name='deck-select'
+            >
+                {deckOptions}
             </select><br />
             <label htmlFor='new-chapter-name' id='new-chapt-name'>New Chapter Name:</label><br />
             <input autoComplete='off' id='new-chapter-name' type='text'></input><br />
@@ -55,6 +91,7 @@ function NewChapterArea({ }) {
 
 function Content({ db }) {
     const [deckNames, setDeckNames] = useState([]);
+    const [chapterNames, setChapterNames] = useState([]);
 
     function addDeck(deckName) {
         db.addDeck(deckName);
@@ -74,15 +111,19 @@ function Content({ db }) {
       <div id='container'>
         <div id='new-chapt-deck'>
             <NewDeckArea onDeckAdd={updateDeckNames} addDeck={addDeck} />
-            <NewChapterArea />
+            <NewChapterArea db={db} deckNames={deckNames} updateChptState={setChapterNames} />
         </div>
-        <AddEditCardArea deckNames={deckNames} />
+        <AddEditCardArea db={db} deckNames={deckNames} chptNamesState={chapterNames} />
       </div>  
     );
 }
 
-function AddEditCardArea({ deckNames }) {
+function AddEditCardArea({ db, deckNames, chptNamesState }) {
+    const [currentDeck, setCurrentDeck] = useState("Deck 0");
+    const [currentChapter, setCurrentChapter] = useState("Chapter 1");
+    // We can create our own hook for these two
     const [deckOptions, setDeckOptions] = useState([]);
+    const [chapterOptions, setChapterOptions] = useState([]);
 
     function updateDeckOptions() {
         // Create a new set of deck options
@@ -94,9 +135,31 @@ function AddEditCardArea({ deckNames }) {
         setDeckOptions(deckOpts);
     }
 
+    function onDeckSelect(e) {
+        // Update state based off select list
+        const selectedDeck = e.target.options[e.target.selectedIndex].value;
+        setCurrentDeck(selectedDeck);
+    }
+
+    function updateChapterOptions() {
+        // Create a new set of chapter options
+        // !! This should just use a state variable called chapterNames?
+        const chapterOpts = db.getChapterNames(currentDeck).map((name, index) => (
+            <option key={index}>{name}</option>
+        ));
+
+        // Update chapterOptions state
+        setChapterOptions(chapterOpts);
+    }
+
+    // When deckNames changes, update the deck select options
     useEffect(() => {
         updateDeckOptions();
-    }, [deckNames])
+    }, [deckNames]);
+
+    useEffect(() => {
+        updateChapterOptions();
+    }, [currentDeck, chptNamesState]);
 
     return (
         <div id='edit-add'>
@@ -107,6 +170,7 @@ function AddEditCardArea({ deckNames }) {
             <div className='select-layout'>
                 <label htmlFor='deck-select'>Select Deck</label>
                 <select
+                    onChange={(e) => onDeckSelect(e)}
                     id='deck-select-edit-add'
                     name='deck-select'
                     className='select-style'
@@ -120,7 +184,7 @@ function AddEditCardArea({ deckNames }) {
                     id='chapter-select'
                     name='chapter-select' 
                     className='select-style'>
-                    {}
+                    {chapterOptions}
                 </select><br />
             </div>
             <div className='select-layout'>
